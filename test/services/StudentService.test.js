7eb5e3fdfcac915a8b7cee07b29d3593a42b418d
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-05-03 20:35:34 -0500

    Agregando función y test getEmail de StudentService

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,11 +1,17 @@
-const Reader = require('./../../lib/utils/Reader')
-const StudentService = require('./../../lib/services/StudentService')
-const studentsJson = 'visualpartners.json'
+const Reader = require("./../../lib/utils/Reader");
+const StudentService = require("./../../lib/services/StudentService");
+const studentsJson = "visualpartners.json";
 
-describe('Test Case: StudentService functions', () => {
-  test('Get all the students', () => {
-    const students =  Reader.readJsonFile(studentsJson)
-    const getThem = StudentService.getAllStudents(students)
-    expect(getThem.length).toBe(51)
-  })
-})
+describe("Test Case: StudentService functions", () => {
+    test("Get all the students", () => {
+        const students =  Reader.readJsonFile(studentsJson);
+        const getThem = StudentService.getAllStudents(students);
+        expect(getThem.length).toBe(51);
+    });
+
+    test("Get all the students with certification", () => {
+        const students = Reader.readJsonFile(studentsJson);
+        const getCert = StudentService.getEmail(students);
+        expect(getCert.length).toBe(29);
+    });
+});
