4af43bb89daf654362812def22184a42d8ee3f9c
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-05-03 20:16:01 -0500

    Test generó error correctamente

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -4,7 +4,6 @@ describe("Test Case: Read the file", () => {
     test("Read the visualpartners.json file", () => {
         const visualpartners = Reader.readJsonFile("visualpartners.json"); 
         console.log(visualpartners); 
-        // expect(Array.isArray(visualpartners)).toBe(true);
-        expect(1).toBe(10)
+        expect(Array.isArray(visualpartners)).toBe(true);
     });
 });
